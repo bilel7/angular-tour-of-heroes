37a00729c2d3fb3bf2b85efaf3a293ced1c96f00
bilel7 <31697833+bilel7@users.noreply.github.com>
2018-03-25 16:00:29 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes test bilel1';
+  title = 'Tour of Heroes';
 }
